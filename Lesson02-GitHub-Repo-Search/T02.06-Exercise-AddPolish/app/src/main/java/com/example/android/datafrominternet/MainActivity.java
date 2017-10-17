b22c17337f9e1b5499b527e03d7ec895388001ba
dipu-bd <dipu.sudipta@gmail.com>
2017-10-18 02:56:45 +0600

    Complete T02.06 with JSON parser]

--- Lesson02-GitHub-Repo-Search/T02.06-Exercise-AddPolish/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.06-Exercise-AddPolish/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -20,13 +20,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.android.datafrominternet.utilities.NetworkUtils;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,9 +43,9 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mSearchResultsTextView;
 
-    // TODO (12) Create a variable to store a reference to the error message TextView
+    private TextView mErrorMessage;
 
-    // TODO (24) Create a ProgressBar variable to store a reference to the ProgressBar
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,9 +57,9 @@ public class MainActivity extends AppCompatActivity {
         mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
 
-        // TODO (13) Get a reference to the error TextView using findViewById
+        mErrorMessage = (TextView) findViewById(R.id.tv_error_message_display);
 
-        // TODO (25) Get a reference to the ProgressBar using findViewById
+        mProgressBar = (ProgressBar) findViewById(R.id.pb_loading_indicator);
     }
 
     /**
@@ -69,12 +76,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // TODO (14) Create a method called showJsonDataView to show the data and hide the error
+    private void showJsonDataView() {
+        mSearchResultsTextView.setVisibility(View.VISIBLE);
+        mErrorMessage.setVisibility(View.INVISIBLE);
+    }
 
     // TODO (15) Create a method called showErrorMessage to show the error and hide the data
+    private void showErrorMessage() {
+        mSearchResultsTextView.setVisibility(View.INVISIBLE);
+        mErrorMessage.setVisibility(View.VISIBLE);
+    }
 
     public class GithubQueryTask extends AsyncTask<URL, Void, String> {
 
-        // TODO (26) Override onPreExecute to set the loading indicator to visible
+        @Override
+        protected void onPreExecute() {
+            mProgressBar.setVisibility(View.VISIBLE);
+            mErrorMessage.setVisibility(View.INVISIBLE);
+            mSearchResultsTextView.setVisibility(View.INVISIBLE);
+        }
 
         @Override
         protected String doInBackground(URL... params) {
@@ -85,17 +105,47 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            return githubSearchResults;
+            return parseJsonResult(githubSearchResults);
+        }
+
+        private String parseJsonResult(String jsonData) {
+            if (jsonData == null || jsonData.equals("")) {
+                return null;
+            }
+
+            String result = "";
+            try {
+                JSONObject data = new JSONObject(jsonData);
+                result += "Total items = " + data.getString("total_count") + "\n\n";
+                JSONArray items = data.getJSONArray("items");
+                int length = Math.min(10, items.length());
+                for(int i = 0; i < length; ++i) {
+                    JSONObject item = (JSONObject) items.get(i);
+                    result += String.format("Repository: %s\n  score = %s (%s stars, %s forks, %s watching).\n  %s\n\n",
+                            item.getString("full_name"),
+                            item.getString("score"),
+                            item.getString("stargazers_count"),
+                            item.getString("forks_count"),
+                            item.getString("watchers_count"),
+                            item.getString("url"));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return result;
         }
 
         @Override
-        protected void onPostExecute(String githubSearchResults) {
-            // TODO (27) As soon as the loading is complete, hide the loading indicator
-            if (githubSearchResults != null && !githubSearchResults.equals("")) {
-                // TODO (17) Call showJsonDataView if we have valid, non-null results
-                mSearchResultsTextView.setText(githubSearchResults);
+        protected void onPostExecute(String results) {
+            mProgressBar.setVisibility(View.INVISIBLE);
+            if (results != null && !results.isEmpty()) {
+                mSearchResultsTextView.setText(results);
+                showJsonDataView();
             }
-            // TODO (16) Call showErrorMessage if the result is null in onPostExecute
+            else {
+                showErrorMessage();
+            }
         }
     }
 
